f0139a0bd8f06e1433928b86bd36dd7d95021d5f
starMaylight <140779753+starMaylight@users.noreply.github.com>
2025-07-31 20:22:38 +0900

    Update gen-pack.sh

--- internal/gen-pack.sh
+++ internal/gen-pack.sh
@@ -27,7 +27,7 @@ cp manifest.json packtmp/
 
 echoerr packtmp/overridesディレクトリを構築
 mkdir packtmp/overrides/
-cp -r config/ fancymenu_data/ groovy/ scripts/ packtmp/overrides/
+cp -r overrides/config/ overrides/fancymenu_data/ overrides/groovy/ overrides/scripts/ packtmp/overrides/
 
 echoerr 既存のpackディレクトリを削除
 rm -rf pack/ || true
