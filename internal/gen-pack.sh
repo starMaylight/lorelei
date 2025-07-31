78387321813a66faf250772b3d716ed1646d15d4
starMaylight <140779753+starMaylight@users.noreply.github.com>
2025-07-31 15:41:25 +0900

    internal

---
+++ internal/gen-pack.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# 出力するファイル名
+PACK_FILE_NAME="lorelei"
+
+# エラー時にスクリプトを停止する
+set -euo pipefail
+
+function echoerr() {
+    echo "$@" 1>&2
+}
+
+echoerr 既存のpacktmpディレクトリを削除
+rm -rf packtmp/ || true
+
+echoerr packtmpディレクトリを構築
+mkdir packtmp/
+cp -r LICENSE modlist.html README.md packtmp/
+
+echoerr packtmp/manifest.jsonを構築
+PACK_VERSION=$(node -e "
+const fs = require('fs');
+const manifest = JSON.parse(fs.readFileSync('manifest.json', 'utf8'));
+console.log(manifest.version);
+")
+cp manifest.json packtmp/
+
+echoerr packtmp/overridesディレクトリを構築
+mkdir packtmp/overrides/
+cp -r config/ kubejs/ multiblocked/ mods/ packtmp/overrides/
+
+echoerr 既存のpackディレクトリを削除
+rm -rf pack/ || true
+
+echoerr packディレクトリを作成
+mkdir pack/
+
+echoerr packtmpディレクトリを圧縮
+cd packtmp/
+zip -qr ../pack/$PACK_FILE_NAME-$PACK_VERSION.zip *
+
+echoerr 保存完了
+echo $PACK_FILE_NAME-$PACK_VERSION
